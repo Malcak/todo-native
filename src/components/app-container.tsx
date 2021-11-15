b2826684bc8affc0910d7f1657053c6bc1fb57dd
Malcak <malcakx@gmail.com>
2021-11-15 02:08:04 -0500

    feat: set theme in native base provider

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { NativeBaseProvider } from 'native-base'
+import theme from '../theme'
 
 type Props = {
   children: React.ReactNode
@@ -9,7 +10,7 @@ type Props = {
 export default function AppContainer({ children }: Props) {
   return (
     <NavigationContainer>
-      <NativeBaseProvider>{children}</NativeBaseProvider>
+      <NativeBaseProvider theme={theme}>{children}</NativeBaseProvider>
     </NavigationContainer>
   )
 }
