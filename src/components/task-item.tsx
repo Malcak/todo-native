97d15a4ccaec8a7b358bc9abd72203f25b06d660
Malcak <malcakx@gmail.com>
2021-11-17 00:41:39 -0500

    feat(task-item): add task-item component

---
+++ src/components/task-item.tsx
@@ -0,0 +1,49 @@
+import React, { useCallback } from 'react'
+import { Pressable } from 'react-native'
+import { Box, useTheme, themeTools, useColorModeValue } from 'native-base'
+import AnimatedCheckbox from 'react-native-checkbox-reanimated'
+
+interface Props {
+  isDone: boolean
+  onToggleCheckbox?: () => void
+}
+
+const TaskItem = (props: Props) => {
+  const { isDone, onToggleCheckbox } = props
+  const theme = useTheme()
+  const highlightColor = themeTools.getColor(
+    theme,
+    useColorModeValue('blue.500', 'blue.400')
+  )
+  const boxStroke = themeTools.getColor(
+    theme,
+    useColorModeValue('muted.300', 'muted.500')
+  )
+  const checkMarkColor = themeTools.getColor(
+    theme,
+    useColorModeValue('white', 'white')
+  )
+  const activeTextColor = themeTools.getColor(
+    theme,
+    useColorModeValue('darkText', 'lightText')
+  )
+  const doneTextColor = themeTools.getColor(
+    theme,
+    useColorModeValue('muted.400', 'muted.600')
+  )
+
+  return (
+    <Box width={30} height={30} mr={2}>
+      <Pressable onPress={onToggleCheckbox}>
+        <AnimatedCheckbox
+          highlightColor={highlightColor}
+          checkmarkColor={checkMarkColor}
+          boxOutlineColor={boxStroke}
+          checked={isDone}
+        />
+      </Pressable>
+    </Box>
+  )
+}
+
+export default TaskItem
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import { Box, Center, Text, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
+import TaskItem from '../components/task-item'
+import { useCallback } from 'react'
 
 export default function MainScreen() {
+  const [checked, setChecked] = React.useState(false)
+  const handlePressCheckbox = React.useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -11,6 +17,9 @@ export default function MainScreen() {
       flex={1}
     >
       <VStack space={5} alignItems="center">
+        <Box>
+          <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
+        </Box>
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello World!</Text>
         </Box>
